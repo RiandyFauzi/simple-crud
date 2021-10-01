6851010868b6ae0b7cb9454e5a86f01d1f7c7aaf
rizalihwan <55536560+rizalihwan@users.noreply.github.com>
2021-10-01 21:13:28 +0700

    Update Siswa.php

--- app/Siswa.php
+++ app/Siswa.php
@@ -7,7 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Siswa extends Model
 {
     protected $table = "siswa";
-    protected $fillable = [
-        'nis', 'nama', 'rombel', 'rayon'
-    ];
+    protected $guarded = [];
 }
